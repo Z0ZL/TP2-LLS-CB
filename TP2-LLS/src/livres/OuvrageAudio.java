ad0478f76b49d4fbecb5d0c8cb254560e920d996
Z0ZL <nobodylol135@gmail.com>
2026-03-27 15:52:20 -0400

    Ajout de la date aux constructeurs des sous-classes de Ouvrage

--- TP2-LLS/src/livres/OuvrageAudio.java
+++ TP2-LLS/src/livres/OuvrageAudio.java
@@ -1,11 +1,13 @@
 package livres;
 
 
+import java.time.LocalDate;
+
 public class OuvrageAudio extends Ouvrage {
     private int dureeMinutes;
     private String format;
 
-    public OuvrageAudio(String titre, Auteur auteur, Format type, int dureeMinutes, String format){
+    public OuvrageAudio(String titre, Auteur auteur, Format type, LocalDate date, int dureeMinutes, String format){
         super(titre, auteur, type);
         this.dureeMinutes = dureeMinutes;
         this.format = format;
